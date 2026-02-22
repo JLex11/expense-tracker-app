ac42b445091680043ed9234cd2b11f7fde07c92a
JLex11 <chikyelfurioso2011@gmail.com>
2026-02-22 00:00:34 -0500

    feat: adding a haptic tab component for reutilization

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -1,18 +1,29 @@
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
-import { PlatformPressable } from '@react-navigation/elements';
-import * as Haptics from 'expo-haptics';
+import type { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
+import * as Haptics from "expo-haptics";
+import { Pressable } from "react-native";
 
-export function HapticTab(props: BottomTabBarButtonProps) {
-  return (
-    <PlatformPressable
-      {...props}
-      onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
-          // Add a soft haptic feedback when pressing down on the tabs.
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-        }
-        props.onPressIn?.(ev);
-      }}
-    />
-  );
+export function HapticTab({
+	onPress,
+	onPressIn,
+	children,
+	style,
+	accessibilityState,
+	accessibilityLabel,
+}: BottomTabBarButtonProps) {
+	return (
+		<Pressable
+			onPress={onPress}
+			onPressIn={(ev) => {
+				if (process.env.EXPO_OS === "ios") {
+					Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+				}
+				onPressIn?.(ev);
+			}}
+			style={style}
+			accessibilityState={accessibilityState}
+			accessibilityLabel={accessibilityLabel}
+		>
+			{children}
+		</Pressable>
+	);
 }
